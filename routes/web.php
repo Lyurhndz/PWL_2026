82627816f7d8e93225668bfd8280cb41d14a0fa4
Lyurhndz <dvnrt.25@gmail.com>
2026-02-18 10:20:23 +0700

    Exercise 1 JS2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+//
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+Route::get('/world', function () {
+    return 'World';
+});
+Route::get('/', function () {
+    return 'welcome';
+});
+Route::get('/about', function () {
+    return '244107020138 - Devin Rianto';
+});
+//
+Route::get('/user/{name}', function ($name) {
+    return 'My name '.$name;
+});
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Post to '.$postId." Comments to: ".$commentId;
+});
+Route::get('/articles/{id}', function ($Id) {
+    return 'Articles Page with ID '.$Id;
+});
+///
+Route::get('/user/{name?}', function ($name=null) {
+    return 'My name '.$name;
+});
+Route::get('/user/{name?}', function ($name='John') {
+    return 'My name '.$name;
+});
+
