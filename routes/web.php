291a99c246890099727dccb3e04a8874064b12e9
Lyurhndz <dvnrt.25@gmail.com>
2026-02-23 23:07:35 +0700

    Exercise 2 JS2

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,53 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
-//
-Route::get('/hello', function () {
-    return 'Hello World';
-});
-Route::get('/world', function () {
-    return 'World';
-});
-Route::get('/', function () {
-    return 'welcome';
-});
+
+//EXEC1 JS2
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
+// Route::get('/world', function () {
+//     return 'World';
+// });
+// Route::get('/', function () {
+//     return 'welcome';
+// });
+// Route::get('/about', function () {
+//     return '244107020138 - Devin Rianto';
+// });
+// //
+// Route::get('/user/{name}', function ($name) {
+//     return 'My name '.$name;
+// });
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Post to '.$postId." Comments to: ".$commentId;
+// });
+// Route::get('/articles/{id}', function ($Id) {
+//     return 'Articles Page with ID '.$Id;
+// });
+// ///
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'My name '.$name;
+// });
+// Route::get('/user/{name?}', function ($name='John') {
+//     return 'My name '.$name;
+// });
+
+//EXEC2 JS2
+Route::get('/hello', [WelcomeController::class,'hello']);
 Route::get('/about', function () {
     return '244107020138 - Devin Rianto';
-});
-//
-Route::get('/user/{name}', function ($name) {
-    return 'My name '.$name;
-});
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Post to '.$postId." Comments to: ".$commentId;
 });
 Route::get('/articles/{id}', function ($Id) {
     return 'Articles Page with ID '.$Id;
 });
-///
-Route::get('/user/{name?}', function ($name=null) {
-    return 'My name '.$name;
-});
-Route::get('/user/{name?}', function ($name='John') {
-    return 'My name '.$name;
-});
 
+use App\Http\Controllers\PhotoController;
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
